57b0237fc1cfefab86b17a103a1fa205619f6857
Abel N. Nieva <931497+abelnnieva@users.noreply.github.com>
2023-12-23 13:06:00 +0000

    prisma database teardown logic

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -13,4 +13,12 @@ export class PrismaService extends PrismaClient {
       },
     });
   }
+
+  cleanDb() {
+    // the elements will be excecuted one by one
+    return this.$transaction([
+      this.bookmark.deleteMany(),
+      this.user.deleteMany(),
+    ]);
+  }
 }
